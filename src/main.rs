ccde53f0f9167a39f7ed18e93ed12c254a42b0ae
Twaha Rahman <mahi6703890@gmail.com>
2020-11-18 13:43:31 +0600

    Update file structure

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     for item in env::args() {
         println!("{}", item);
     }
-    let mut before = image::open("before.png").unwrap();
-    let mut after = image::open("after.png").unwrap();
+    let mut before = image::open("samples/before.png").unwrap();
+    let mut after = image::open("samples/after.png").unwrap();
 
     let diff = compare(&mut before, &mut after, 100.0 / 256.0).unwrap();
 
-    before.save("marked_before.png").unwrap();
-    after.save("marked_after.png").unwrap();
-    diff.save("diff.png").unwrap();
+    before.save("output/marked_before.png").unwrap();
+    after.save("output/marked_after.png").unwrap();
+    diff.save("output/diff.png").unwrap();
 }
